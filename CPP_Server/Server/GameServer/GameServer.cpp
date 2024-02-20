faae5e042fa1dfd3149b4aeee1f15807f0740fd2
JeongYoonseong <smfgkgks123@gmail.com>
2024-02-20 21:13:25 +0900

    Memory Model 정책

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -4,55 +4,47 @@
 #include <mutex>
 #include <windows.h>
 #include <future>
-atomic<bool> flag = false;
+
+atomic<bool> ready;
+int32 value;
+
+void Producer()
+{
+	value = 10;
+	ready.store(true, memory_order::memory_order_release);
+}
+
+void Consumer()
+{
+	while (ready.load(memory_order::memory_order_acquire) == false)
+		;
+
+	cout << value << endl;
+}
+
 int main()
 {
-	flag = false;
+	ready = false;
+	value = 0;
+	thread t1(Producer);
+	thread t2(Consumer);
+	t1.join();
+	t2.join();
+	//Memory Model 정책
+	//1)Sequentially Consistent(seq_cst)
+	//2)Acquire-Release(consume,acquire,release,acq_rel)
+	//3)Relaxed(relaxed)
 
-	//if (flag.is_lock_free())
-	//{
-	//	//원래 원자적 처리 가능
-	//}
-	{
-		flag = true;
-		bool val = flag;
-	}
-	{
-		flag.store(true);
-		bool val = flag.load();
-	}
-	{
-		flag.store(true,memory_order::memory_order_seq_cst);
-		bool val = flag.load(memory_order::memory_order_seq_cst);
-	}
+	// 1) seq_cst(컴파일러 최적화 여지 적음 = 직관적 = 가장 엄격)
+	// 가시성문제 해결 코드 재배치 문제 해결
 
-	{
-		bool prev = flag; // 다른 쓰레드에서 쓰고있으면 유효하지 않을 수 있음
-		flag = true;
 
-	}
-	// ->
-	{
-		bool prev = flag.exchange(true);
-	}
-
-	//CAS 조건부 수정
-	{
-		bool expected = false;
-		bool desired = true;
-		flag.compare_exchange_strong(expected, desired);
-
-		if (flag == expected)
-		{
-			flag = desired;
-			return true;
-		}
-		else
-		{
-			expected = flag;
-			return false;
-		}
-		// 사용법 똑같지만, Spurious Failure 문제가 있을 수 있음. while문 써서 사용할 것
-		flag.compare_exchange_weak(expected, desired);
-	}
+	// 2) acquire-release 
+	// // release 명령 이전의 메모리 명령들이 해당 명령 이후로 재배치 되는걸 막음
+	//acquire로 같은 변수를 읽는 쓰레드가 있다면 release 이전의 명령들이 acquire 하는 순간부터 관찰 가능(가시성)
+	// 
+	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+	// 코드 재배치 멋대로, 가시성 해결 x
+	// 동일 객체에 대한 동일 관전 순서만 보장
+	
 }
