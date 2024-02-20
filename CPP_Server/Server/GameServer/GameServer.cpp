0256bdd7714c267f4857c4635054e5fd6ae66943
JeongYoonseong <smfgkgks123@gmail.com>
2024-02-20 19:04:23 +0900

    condition_variable을 이용한 Lock 처리

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -8,19 +8,28 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// User-Level Object(커널 오브젝트 X)
+condition_variable cv;
 
+//#include <condition_variable>
+//condition_variable_any cv;
 
 void Producer()
 {
 	while (true)
 	{
+		//Lock과 짝을 지어서 사용
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값 수정
+		// 3) Lock 해제
+		// 4) 조건변수를 통해 다른 쓰레드에 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
-		this_thread::sleep_for(100ms);
+		cv.notify_one(); // wait중인 쓰레드가 있으면 하나만 깨운다
 	}
 }
 
@@ -28,12 +37,16 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
-		// Auto인 경우 Non-Signal 상태로 변환
-		// Manual인 경우
-		::ResetEvent(handle);
+
 		unique_lock<mutex> lock(m);
-		if (!q.empty())
+		// 중간에 lock풀어주는 기능이 있어야해서 Lock_Guard 사용 불가
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡고
+		// 2) 조건 확인
+		// 3) 만족하면 빠져나와 코드 진행
+		// 4) 만족하지 않는다면, Lock을 풀고 대기로 전환
+
+		
 		{
 			int32 data = q.front();
 			q.pop();
@@ -48,7 +61,7 @@ int main()
 	// Usage Count
 	// Signal(파란불) / Non-Signal(빨간불) << bool
 	// Auto / Manual Reset << bool
-	handle = ::CreateEvent(NULL/*보안속성*/, TRUE, FALSE, NULL);
+	//handle = ::CreateEvent(NULL/*보안속성*/, TRUE, FALSE, NULL);
 
 	thread t1(Producer);
 	thread t2(Consumer);
@@ -56,5 +69,5 @@ int main()
 	t1.join();
 	t2.join();
 
-	::CloseHandle(handle);
+	//::CloseHandle(handle);
 }
