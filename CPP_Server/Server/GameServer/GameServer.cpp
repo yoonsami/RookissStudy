a1cd8b677129ecd9d62e2dbde6520ae5cd479839
JeongYoonseong <smfgkgks123@gmail.com>
2024-02-22 21:18:15 +0900

    TLS(Thread Local Storage)를 이용해 Thread ID 발급하기 실습
    Thread마다 가지고있는 영역을 만들어준다

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -5,46 +5,31 @@
 #include <windows.h>
 #include <future>
 
-atomic<bool> ready;
-int32 value;
+//쓰레드마다 고유하게 가질 정보가 된다
+thread_local int32 LThreadId = 0;
+thread_local queue<int32> q;
 
-void Producer()
+void ThreadMain(int32 threadId)
 {
-	value = 10;
-	ready.store(true, memory_order::memory_order_release);
-}
+	LThreadId = threadId;
 
-void Consumer()
-{
-	while (ready.load(memory_order::memory_order_acquire) == false)
-		;
-
-	cout << value << endl;
+	while (true)
+	{
+		cout << "I am Thread " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
 }
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
-	//Memory Model 정책
-	//1)Sequentially Consistent(seq_cst)
-	//2)Acquire-Release(consume,acquire,release,acq_rel)
-	//3)Relaxed(relaxed)
+	vector<thread> threads;
+	for (int32 i = 0; i < 10; ++i)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
 
-	// 1) seq_cst(컴파일러 최적화 여지 적음 = 직관적 = 가장 엄격)
-	// 가시성문제 해결 코드 재배치 문제 해결
-
-
-	// 2) acquire-release 
-	// // release 명령 이전의 메모리 명령들이 해당 명령 이후로 재배치 되는걸 막음
-	//acquire로 같은 변수를 읽는 쓰레드가 있다면 release 이전의 명령들이 acquire 하는 순간부터 관찰 가능(가시성)
-	// 
-	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
-	// 코드 재배치 멋대로, 가시성 해결 x
-	// 동일 객체에 대한 동일 관전 순서만 보장
+	for (thread& t : threads)
+		t.join();
 	
 }
