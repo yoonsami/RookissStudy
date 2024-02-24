adb463978807d54942ee259077269da5ec34425b
JeongYoonseong <smfgkgks123@gmail.com>
2024-02-24 10:51:57 +0900

    shader_ptr을 이용한 GarbageCollect
    shared_ptr이 lock-free방식으로 동작한다는 보장이 읍다
    atomic_is_lock_free 로 확인해보자
    
    동작은 하는데 lock-free가 아니다

--- CPP_Server/Server/GameServer/ConcurrentStack.h
+++ CPP_Server/Server/GameServer/ConcurrentStack.h
@@ -56,10 +56,10 @@ class LockFreeStack
 {
 	struct Node
 	{
-		Node(const T& value) : data(value),next(nullptr), _pendingList(nullptr)
+		Node(const T& value) : data(make_shared<T>(value)),next(nullptr)
 		{}
-		T data;
-		Node* next;
+		shared_ptr<T> data;
+		shared_ptr<Node> next;
 	};
 
 public:
@@ -68,120 +68,35 @@ public:
 		//1) 노드 생성
 		//2) 새 노드의 next를 헤드로
 		//3) 헤드를 새 노드로
-		Node* node = new Node(value);
-		node->next = _head;
-		
-		while (_head.compare_exchange_weak(node->next, node) == false)
+		shared_ptr<Node> node = make_shared<Node>(value);
+		node->next = std::atomic_load(&_head);
+
+		while (std::atomic_compare_exchange_weak(&_head, &node->next, node) == false)
 		{
-
 		}
-
 	}
 
-	bool TryPop(T& value)
+	shared_ptr<T> TryPop()
 	{
-
-
 		//1)Head 읽기 -> 읽기부터 문제 될 수 있따
 		//2)Head->next읽기
 		//3)head = head->next
 		//4)data추출 반환
 		//5) 추출 노드 삭제
+	
+		shared_ptr<Node> oldHead = std::atomic_load(&_head);
 
-		++_popCount;
-
-		Node* oldHead = _head;
-		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
+		while (oldHead && std::atomic_compare_exchange_weak(&_head, &oldHead, oldHead->next) == false)
 		{
-
 		}
 
 		if (oldHead == nullptr)
-		{
-			--_popCount;
-			return false;
-		}
+			return shared_ptr<T>();
 
-		//Exception X
-		value = oldHead->data;
-		// GARBAGE Collector 잇으면 끝
-		//없으면 누수 잡아야함
-		// 잠시 삭제 보류
-		TryDelete(oldHead);
-
-		return true;
-	}
-
-	void TryDelete(Node* oldHead)
-	{
-		// 나 외에 누가 있는가
-		if (_popCount == 1)
-		{
-			//혼자니까, 예약된 다른 데이터도 삭제해보자
-			Node* node = _pendingList.exchange(nullptr);
-
-			if (--_popCount == 0)
-			{
-				//아직도 혼자라면 -> 삭제 가능
-				//여기서 누가 끼어들어도 이미 데이터 분리해서 상관ㄴ
-				DeleteNodes(node);
-			}
-			else if(node)
-			{
-				//누가 끼어들었으니 다음에 하자.
-				ChainPendingNodeList(node);
-
-			}
-
-			delete oldHead;
-		}
-		else
-		{
-			// 누가 있으니까, 삭제 예약만
-			ChainPendingNode(oldHead);
-			--_popCount;
-		}
-	}
-
-	void ChainPendingNodeList(Node* first, Node* last)
-	{
-		last->next = _pendingList;
-
-		//Compare and Swap
-		while (_pendingList.compare_exchange_weak(last->next,first) == false)
-		{
-
-		}
-	}
-
-	void ChainPendingNodeList(Node* node)
-	{
-		Node* last = node;
-		while (last->next)
-			last = last->next;
-
-		ChainPendingNodeList(node, last);
-	}
-
-	void ChainPendingNode(Node* node)
-	{
-		ChainPendingNodeList(node, node);
-	}
-
-	static void DeleteNodes(Node* node)
-	{
-		while (node)
-		{
-			Node* next = node->next;
-			delete node;
-			node = next;
-		}
+		return oldHead->data;
 	}
 
 private:
-	atomic<Node*> _head;
-
-	atomic<uint32> _popCount = 0; // Pop 실행중인 쓰레드 개수
-	atomic<Node*> _pendingList; //삭제 되어야 할 노드들
+	shared_ptr<Node> _head;
 
 };
