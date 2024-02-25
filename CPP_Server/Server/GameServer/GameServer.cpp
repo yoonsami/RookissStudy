81e02f4a0a87420bda2e79983073dbddd5e2fda2
JeongYoonseong <smfgkgks123@gmail.com>
2024-02-25 15:08:44 +0900

    멀티쓰레드 환경에서의 RefCount 관리
    멀티쓰레드 환경에서는 new로 할당하고 직접 AddRef호출하는 등 중간 중간 다른 쓰레드의 개입 여지가 있기 때문에,
    smartPtr처럼 스스로 RefCount를 증가시키거나 하는 방법이 필요하다.

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -1,49 +1,8 @@
 ﻿#include "pch.h"
 #include "ThreadManager.h"
 
-// 소수 구하기 문제
-atomic<int32> ans;
-
-bool IsPrime(int32 num)
-{
-	if (num <= 1) return false;
-	if (num == 2 || num == 3) true;
-
-	for (int32 i = 2; i * i <= num; ++i)
-		if (num % i == 0) return false;
-
-	return true;
-}
-
-void Thread(int32 start, int32 end)
-{
-	for (int i = start; i < end; ++i)
-	{
-		if (IsPrime(i)) ans++;
-
-
-	}
-
-
-
-}
-
-
 int main()
 {
-	ans = 0;
-	const int MAX_NUMBER = 1000000;
 
-	vector<thread> v;
-	for (int32 i = 0; i < 1000; ++i)
-	{
-		v.push_back(thread(Thread, i * 1000, (i + 1) * 1000));
 
-	}
-	for (int32 i = 0; i < 1000; ++i)
-	{
-		v[i].join();
-	}
-
-	cout << ans << endl;
 }
